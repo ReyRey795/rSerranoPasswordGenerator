edd29b20b2f6be7df87ef039e38ac2c57ce8066f
Rey Serrano <serranoj.rreynaldo@gmail.com>
2021-08-14 17:37:34 -0500

    Added line 29 to link to the length id from html.

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -5,7 +5,7 @@ function generatePassword(){
   //Will hold random characters needed
   var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*()_+=-";
   // set password length
-  var length = document.getElementById("slider").value;
+  var lengthChart = document.getElementById("slider").value;
 
   var password = "";
 
@@ -13,7 +13,7 @@ function generatePassword(){
   //Math.floor will return the largest integer. Math.random will choose a number between 0 and 1, by multiplying it by the value length you will git a hole number,
   //and because index begins a 0, you will need to subtract by 1 to set everything back by one, otherwise you will never get 0 or you will get a index number that
   //is not in the array.
-  for(var i = 0; i <= length; i++){
+  for(var i = 0; i <= lengthChart; i++){
     password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
   }
   //Add password to the textbox. What is seen in the box.
@@ -23,3 +23,7 @@ function generatePassword(){
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
+
+//set default length display to vary from 8 characters to 128.
+
+document.getElementById("length").innerHTML = "Length: 128";
