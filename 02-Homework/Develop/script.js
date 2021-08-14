cb9dcd2097b835da43006ad3ac2497bf9d2e3bac
Rey Serrano <serranoj.rreynaldo@gmail.com>
2021-08-14 17:28:38 -0500

    Removed lines 24 - 31 to see if it would work to generate the random password and it did.

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -21,15 +21,5 @@ function generatePassword(){
   passwordText.value = password;
 }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
