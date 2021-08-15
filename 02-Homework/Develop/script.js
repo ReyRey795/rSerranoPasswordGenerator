9ce30c3a5d3ce360091cd9f4444f3d5008939b0c
Rey Serrano <serranoj.rreynaldo@gmail.com>
2021-08-14 20:02:52 -0500

    Removed the equals sign on line 16 in JS, and resituated the default display number to 60. Removed else statement from the bottom function. Characters are now displaying correct number.

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -13,17 +13,25 @@ function generatePassword(){
   //Math.floor will return the largest integer. Math.random will choose a number between 0 and 1, by multiplying it by the value length you will git a hole number,
   //and because index begins a 0, you will need to subtract by 1 to set everything back by one, otherwise you will never get 0 or you will get a index number that
   //is not in the array.
-  for(var i = 0; i <= lengthChart; i++){
+  for(var i = 0; i < lengthChart; i++){
     password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
   }
   //Add password to the textbox. What is seen in the box.
   document.getElementById("password").value = password;
-  passwordText.value = password;
+  passwordText = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
 
-//set default length display to vary from 8 characters to 128.
+//set default length display to 60 so it split evenly between numbers.
 
-document.getElementById("length").innerHTML = "Length: 128";
+document.getElementById("length").innerHTML = "Length: 60";
+
+//Create a function to set length based on the slide.
+
+document.getElementById("slider").oninput = function(){
+  if(document.getElementById("slider").value >0){
+    document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
+  }
+}
